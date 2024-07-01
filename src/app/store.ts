b5a56584c65d1456e1e3d0cba4e51c5fa94ae3fa
Victor <pobozhny94@gmail.com>
2024-07-01 22:30:11 +0300

    5.1.2

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,27 +1,31 @@
-import { tasksReducer } from "features/TodolistsList/tasks-reducer";
-import { todolistsReducer } from "features/TodolistsList/todolists-reducer";
-import { combineReducers } from "redux";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
-import { appReducer } from "./app-reducer";
-import { authReducer } from "features/Login/auth-reducer";
-import { configureStore, UnknownAction } from "@reduxjs/toolkit";
+import {tasksReducer} from "features/TodolistsList/tasks-reducer";
+import {todolistsReducer} from "features/TodolistsList/todolists-reducer";
+import {combineReducers} from "redux";
+import {ThunkAction} from "redux-thunk";
+import {appReducer} from "./app-reducer";
+import {authReducer} from "features/Login/auth-reducer";
+import {configureStore, UnknownAction} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
 });
 
 // ❗старая запись, с новыми версиями не работает
 //  const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-export const store = configureStore({ reducer: rootReducer });
+export const store = configureStore({reducer: rootReducer});
 
-export type AppRootStateType = ReturnType<typeof rootReducer>;
-
+export type AppRootStateType = ReturnType<typeof store.getState>
 // ❗ UnknownAction вместо AnyAction
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, UnknownAction>;
 
 // export type AppDispatch = typeof store.dispatch
 // ❗ UnknownAction вместо AnyAction
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>;
+//export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>;
+
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+
