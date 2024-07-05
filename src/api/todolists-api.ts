a65ebf95a29ad0ed9c909d9ad7b6b9872c6a7ada
Victor <pobozhny94@gmail.com>
2024-07-05 23:42:09 +0300

    5.1.2

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -73,17 +73,11 @@ export type ResponseType<D = {}> = {
 
 export enum TaskStatuses {
     New = 0,
-    InProgress = 1,
     Completed = 2,
-    Draft = 3,
 }
 
 export enum TaskPriorities {
     Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4,
 }
 
 export type TaskType = {
